8bcf08c86914c98f02c932ef0077856e0e4ed662
Tempest <93183950+Ryan-Pienaar@users.noreply.github.com>
2022-09-09 01:41:41 +0200

    Writes verified long, lat and google place name back to *.xlsx file

--- main.py
+++ main.py
@@ -2,11 +2,16 @@ import pandas as pd
 import sys
 import requests
 
-FILE = sys.argv[1]
-SheetName = sys.argv[2]
-API_KEY = 'AIzaSyB07eD8KQyzpMuU-_cGF48ax59gWWahTL8'
+FILE = sys.argv[1]  # *.xlsx file path to be GeoCoded
+SheetName = sys.argv[2]  # Sheet of *.xlsx File to be used
+API_KEY = 'AIzaSyB07eD8KQyzpMuU-_cGF48ax59gWWahTL8'  # Ryan Pienaar Google API Key !DO NOT USE!
 
-def getGeoCoord(address):
+xlsx = pd.read_excel(FILE, sheet_name=SheetName)  # Creates DataFrame from *.xlsx file
+
+
+# Uses Google API To get the longitude and latitude of a place name
+# Returns found longitude and latitude
+def getLongLat(address):
     params = {
         'key': API_KEY,
         'address': address.replace(' ', '+')
@@ -15,11 +20,14 @@ def getGeoCoord(address):
     base_url = 'https://maps.googleapis.com/maps/api/geocode/json?'
     response = requests.get(base_url, params=params)
     data = response.json()
+    #print(data)
     if data['status'] == 'OK':
         result = data['results'][0]
         location = result['geometry']['location']
+        name = result['formatted_address']
         print("Latitude: " + str(location['lat']) + " Longitude: " + str(location['lng']))
-        return location['lat'], location['lng']
+
+        return location['lng'], location['lat'], name,
     else:
         return
 
@@ -30,13 +38,16 @@ def printExcel(file):
 #printExcel(FILE)
 
 
-def parseLocationNames(file):
-    xlsx = pd.read_excel(file)
-
+# Verifies found Coordinates
+def verifyCoord(file):
     for ind in xlsx.index:
         print(xlsx['PlaceLabel'][ind])
-        getGeoCoord(xlsx['PlaceLabel'][ind])
+        lat, long, name = getLongLat(xlsx['PlaceLabel'][ind])
+        xlsx.at[ind, 'VerifiedLat'] = lat
+        xlsx.at[ind, 'VerifiedLong'] = long
+        xlsx.at[ind, 'Google Place Name'] = name
 
-
-
-parseLocationNames(FILE)
+# Driving code
+verifyCoord(FILE)
+print(xlsx)
+xlsx.to_excel(FILE, sheet_name=SheetName)  # Writes DataFrame back to Excel file
